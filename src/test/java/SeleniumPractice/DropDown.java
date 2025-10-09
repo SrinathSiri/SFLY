9ed35d53d9d9b5ec8aeb45cd039669a7d7398b63
SrinathSiri <167597003+SrinathSiri@users.noreply.github.com>
2025-10-09 11:23:55 +0530

    Update DropDown.java
    removed 'on' word

--- src/test/java/SeleniumPractice/DropDown.java
+++ src/test/java/SeleniumPractice/DropDown.java
@@ -13,7 +13,7 @@ import java.time.Duration;
 public class DropDown {
     @Test
     public void dropDownSelect() throws Exception{
-        // Dropdown practice on leafground site
+        // Dropdown practice leafground site
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
