097aa5f002ee64d3296f858756f93291bd9eb507
v-srinath.sirimalla <srinathsirimalla18@gmail.com>
2025-12-19 16:46:16 +0530

    ArrayEx1 commit

--- src/test/java/JavaPractice/ArrayEx1.java
+++ src/test/java/JavaPractice/ArrayEx1.java
@@ -15,7 +15,7 @@ public class ArrayEx1 {
         data[2][1] = "Playwright";
         data[2][2] = "JavaScript";
 
-        System.out.println(data[0][0]);
+        /*System.out.println(data[0][0]);
         System.out.println(data[0][1]);
         System.out.println(data[0][2]);
         System.out.println(data[1][0]);
@@ -23,6 +23,16 @@ public class ArrayEx1 {
         System.out.println(data[1][2]);
         System.out.println(data[2][0]);
         System.out.println(data[2][1]);
-        System.out.println(data[2][2]);
+        System.out.println(data[2][2]);*/
+
+        for(int i=0;i<data.length;i++){
+            for(int j=0;j<data[i].length;j++){
+                System.out.print(data[i][j] + " | ");
+            }
+            System.out.println("");
+        }
+
+
+
     }
 }
