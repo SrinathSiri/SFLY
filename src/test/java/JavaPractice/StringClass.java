d31e143c752440d26541d3b36a769a12eda2bbad
v-srinath.sirimalla <srinathsirimalla18@gmail.com>
2026-01-01 19:52:55 +0530

    String content 01012026

--- src/test/java/JavaPractice/StringClass.java
+++ src/test/java/JavaPractice/StringClass.java
@@ -4,5 +4,15 @@ public class StringClass {
     public static void main(String[] args){
         String s="";
         System.out.println(s);
+
+        String obj = new String("PAVAN");
+        System.out.println(obj);
+
+        obj = obj+"Kalyan";
+        System.out.println("After + operator is : "+obj); // New object created
+
+        //obj = obj.concat("KALYAN");
+        //System.out.println("After concat : "+obj);
+
     }
 }
