228b5f3e9a4adebdaf4485d4039313711745ee6b
v-srinath.sirimalla <srinathsirimalla18@gmail.com>
2026-01-03 16:50:48 +0530

    .equal == 03012026

---
+++ src/test/java/JavaPractice/StringEqualDouble.java
@@ -0,0 +1,14 @@
+package JavaPractice;
+
+public class StringEqualDouble {
+
+    public static void main(String[] args) {
+
+        String s1 = "Hello";
+        String s2 = s1 + "World";
+
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
+
+    }
+}
